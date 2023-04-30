fa4243d453bd3054e1669e7a7e7229fd4771f6d4
Gerardo Cianciulli <gerardo.cianciulli@gmail.com>
2023-04-30 13:10:53 -0400

    add focus function

--- rating-component.js
+++ rating-component.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-    $(".round-label").click(function(e){
-      var selected_input = $($(e.target).find("input")),
+
+    function selectScore(e) {
+        var selected_input = $(e.target),
         score_val = selected_input.val();
-      if(score_val != undefined) {
-        $("#view-score span").text(score_val)
-        $(".round-label").removeClass("selected");
-        selected_input.parent().addClass("selected");
-      }
-    });
+        
+        if(score_val != undefined) {
+            $("#view-score span").text(score_val)
+            $(".round-label").removeClass("selected");
+            selected_input.parent().addClass("selected");
+        }
+    }
+
+    $(".round-label").click(selectScore);
+
+    $(".round-label input").on( "focus", selectScore);
+
     $("#score .submit").click(function(e){
         if ($(".round-label.selected").length == 1) {
             $("#score-card").hide();
